95e7ff21d62d89432c8ab3448c60214d0f7ca477
chitra atmakuru & bryan borgeson <student@epicodus.com>
2015-03-18 12:14:33 -0700

    Write getAll function and make all tests pass except for test_save.  Must write deleteAll method to pass test_save.

--- src/Restaurant.php
+++ src/Restaurant.php
@@ -15,6 +15,7 @@
             $this->cuisine_id = $cuisine_id;
         }
 
+        //SETTERS
         function setId($new_id)
         {
             $this->id = $new_id;
@@ -30,6 +31,7 @@
             $this->cuisine_id = $new_cuisine_id;
         }
 
+        // GETTERS
         function getId()
         {
             return $this->id;
@@ -50,6 +52,20 @@
             $statement = $GLOBALS['DB']->query("INSERT INTO restaurants (name, cuisine_id) VALUES ('{$this->getName()}', {$this->getCuisineId()}) RETURNING id;");
             $result = $statement->fetch(PDO::FETCH_ASSOC);
             $this->setId($result['id']);
+        }
+
+        static function getAll()
+        {
+            $returned_restaurants = $GLOBALS['DB']->query("SELECT * FROM restaurants;");
+            $restaurants = array();
+            foreach ($returned_restaurants as $restaurant) {
+                $name = $restaurant['name'];
+                $id = $restaurant['id'];
+                $cuisine_id = $restaurant['cuisine_id'];
+                $new_restaurant = new Restaurant($name, $id, $cuisine_id);
+                array_push($restaurants, $new_restaurant);
+            }
+            return $restaurants;
         }
 
     }
--- tests/RestaurantTest.php
+++ tests/RestaurantTest.php
@@ -8,7 +8,7 @@
     require_once "src/Restaurant.php";
     require_once "src/Cuisine.php";
 
-    $DB = new PDO('pgsql:host=localhost;dbname=places_to_eat');
+    $DB = new PDO('pgsql:host=localhost;dbname=test_places_to_eat');
 
     class RestaurantTest extends PHPUnit_Framework_TestCase
     {
@@ -36,6 +36,112 @@
             //Assert
             $this->assertEquals(true, is_numeric($result));
         }
+
+        function test_getCuisineId()
+        {
+            //Arrange
+            $food_type = "Ramen Food";
+            $id = null;
+            $test_cuisine = new Cuisine($food_type, $id);
+            $test_cuisine->save();
+
+            $name = "McDonalds";
+            $cuisine_id = $test_cuisine->getId();
+            $test_restaurant = new Restaurant($name, $id, $cuisine_id);
+            $test_restaurant->save();
+
+            //Act
+            $result = $test_restaurant->getCuisineId();
+
+            //Assert
+            $this->assertEquals(true, is_numeric($result));
+        }
+
+        function test_setId()
+        {
+            //Arrange
+            $food_type = "Fast Food";
+            $id = null;
+            $test_cuisine = new Cuisine($food_type, $id);
+            $test_cuisine->save();
+
+            $name = "McDonalds";
+            $cuisine_id = $test_cuisine->getId();
+            $test_restaurant = new Restaurant($name, $id, $cuisine_id);
+            $test_restaurant->save();
+
+            //Act
+            $test_restaurant->setId(2);
+
+            //Assert
+            $result = $test_restaurant->getId();
+
+            $this->assertEquals(2, $result);
+        }
+
+        function test_setName()
+        {
+            //Arrange
+            $food_type = "Fast Food";
+            $id = null;
+            $test_cuisine = new Cuisine($food_type, $id);
+            $test_cuisine->save();
+
+            $name = "McDonalds";
+            $cuisine_id = $test_cuisine->getId();
+            $test_restaurant = new Restaurant($name, $id, $cuisine_id);
+            $test_restaurant->save();
+
+            //Act
+            $test_restaurant->setName("Bob's Burgers");
+
+            //Assert
+            $result = $test_restaurant->getName();
+
+            $this->assertEquals("Bob's Burgers", $result);
+        }
+
+
+        function test_getName()
+        {
+            //Arrange
+            $food_type = "Fast Food";
+            $id = null;
+            $test_cuisine = new Cuisine($food_type, $id);
+            $test_cuisine->save();
+
+            $name = "McDonalds";
+            $cuisine_id = $test_cuisine->getId();
+            $test_restaurant = new Restaurant($id, $name, $cuisine_id);
+
+            //Act
+            $result = $test_restaurant->getName();
+
+            //Assert
+            $this->assertEquals("McDonalds", $result);
+        }
+
+
+        function test_save()
+        {
+            //Arrange
+            $food_type = "Fast Food";
+            $id = null;
+            $test_cuisine = new Cuisine($food_type, $id);
+            $test_cuisine->save();
+
+            $name = "Taco Bell";
+            $cuisine_id = $test_cuisine->getId();
+            $test_restaurant = new Restaurant($id, $name, $cuisine_id);
+
+            //Act
+            $test_restaurant->save();
+
+            //Assert
+            $result = Restaurant::getAll();
+            $this->assertEquals($test_restaurant, $result[0]);
+        }
+
     }
 
 ?>
